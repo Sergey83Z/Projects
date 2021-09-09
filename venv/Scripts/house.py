69f08535c619915658df717273540b571d6bd123
Захаров Сергей Валерьевич <sergey.zaharov@s-telematics.ru>
2021-09-09 09:55:08 +0300

    Initial commit

---
+++ venv/Scripts/house.py
@@ -0,0 +1,17 @@
+
+def draw_house(x, y, width, heigth):
+    """
+    Нарисовать домик высотой heigth и широной width от опорной точки
+    находящейся в середине фундамента
+    :param x:
+    :param y:
+    :param width:
+    :param heigth:
+    :return:
+    """
+    pass
+
+x, y = 100, 100
+width, heigth = 200, 200
+
+draw_house(x,y,width,heigth)
